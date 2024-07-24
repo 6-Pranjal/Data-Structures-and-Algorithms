627e97a663723cb30910b8f6a2109fac3cb794a4
Prranjal_jatav <163637769+6-Pranjal@users.noreply.github.com>
2024-07-25 00:35:17 +0530

    Add files via upload

---
+++ 39_for_each.java
@@ -0,0 +1,16 @@
+/**
+ * for_each
+ */
+public class for_each {
+
+  public static void main(String[] args) {
+
+    int[] arr = { 1, 4, 5, 7, 8, 9 };
+
+    for (int i : arr) {
+      System.out.print(i);
+      System.out.print(" ");
+    }
+    
+  }
+}
